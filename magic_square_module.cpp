a85098a969ac34d92daf71c0fcb805f3388ff815
matb22 <deddidmide@gmail.com>
2026-01-07 19:13:33 +0500

    add check magic square func

--- magic_square_module.cpp
+++ magic_square_module.cpp
@@ -10,6 +10,7 @@
 void Output_square_console(int n , std::vector<std::vector<int>> magicSquare) ; 
 void Output_square_file(int n , std::vector<std::vector<int>> magicSquare);
 int countDigits(int n  )  ; 
+bool isMagicSquareValid(const std::vector<std::vector<int>>& square) ; 
 
 void generateMagicSquare(int n) {
   
@@ -34,6 +35,19 @@ void generateMagicSquare(int n) {
       j = next_j;
     }
   }
+
+  if (isMagicSquareValid(magicSquare)) {
+        std::cout << "✓ Квадрат корректный!\n";
+    } 
+  else {
+        std::cout << "✗ Ошибка: квадрат некорректный!\n";
+    }
+
+
+
+
+
+
   Output_square_console(n , magicSquare) ; 
   Output_square_file( n , magicSquare) ; 
   
@@ -102,3 +116,58 @@ void Square_info() {
 
 
 
+
+
+bool isMagicSquareValid(const std::vector<std::vector<int>>& square) {
+    int n = square.size();
+    if (n % 2 == 0) return false; 
+    
+    // Проверяем, что все числа от 1 до n² присутствуют ровно один раз
+    std::vector<bool> found(n * n + 1, false);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            int num = square[i][j];
+            if (num < 1 || num > n * n || found[num]) {
+                return false;
+            }
+            found[num] = true;
+        }
+    }
+    
+    // Вычисляем магическую константу
+    int magicSum = n * (n * n + 1) / 2;
+    
+    // Проверяем суммы строк
+    for (int i = 0; i < n; i++) {
+        int rowSum = 0;
+        for (int j = 0; j < n; j++) {
+            rowSum += square[i][j];
+        }
+        if (rowSum != magicSum) return false;
+    }
+    
+    // Проверяем суммы столбцов
+    for (int j = 0; j < n; j++) {
+        int colSum = 0;
+        for (int i = 0; i < n; i++) {
+            colSum += square[i][j];
+        }
+        if (colSum != magicSum) return false;
+    }
+    
+    // Проверяем главную диагональ
+    int diag1Sum = 0;
+    for (int i = 0; i < n; i++) {
+        diag1Sum += square[i][i];
+    }
+    if (diag1Sum != magicSum) return false;
+    
+    // Проверяем побочную диагональ
+    int diag2Sum = 0;
+    for (int i = 0; i < n; i++) {
+        diag2Sum += square[i][n - 1 - i];
+    }
+    if (diag2Sum != magicSum) return false;
+    
+    return true;
+}

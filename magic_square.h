fd7c51c5b2a88d849475237957458f20bdd6e16a
matb22 <deddidmide@gmail.com>
2026-01-08 16:43:49 +0500

    fix magic_square.h

--- magic_square.h
+++ magic_square.h
@@ -1,13 +1,16 @@
 #ifndef MAGIC_SQUARE_H
 #define MAGIC_SQUARE_H
 
+
+
+#include "magic_square_module.cpp" 
 void DisplaySquareInfo() ; 
 void OutputSquareConsole(int n, const std::vector<std::vector<int>>& magic_square);
 void OutputSquareFile(int n, const std::vector<std::vector<int>>& magic_square);
 int CountDigits(int number);
 bool IsMagicSquareValid(const std::vector<std::vector<int>>& square);
 void GenerateMagicSquare(int square_size) ; 
-#include "magic_square_module.cpp" 
+
 
 
 
